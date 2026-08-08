e73c792c13534c00b4d1e7b3c87b33f101f420c8
Nashrah <nashrah0607@gmail.com>
2026-08-08 11:36:38 +0530

    events emitter

---
+++ 13-event-emitter.js
@@ -0,0 +1,13 @@
+const eventEmitter = require('events')
+
+const customEmitter = new eventEmitter()
+
+customEmitter.on('response', (name, id)=>{
+  console.log( `data received user ${name} id ${id}`)
+})
+
+customEmitter.on('response', ()=>{
+  console.log( `some other logic`)
+})
+
+customEmitter.emit('response', 'john', 1)
