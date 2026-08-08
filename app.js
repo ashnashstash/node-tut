d030cad7e771ac05b47304c9d4863deb97aebb37
Nashrah <nashrah0607@gmail.com>
2026-08-08 11:54:13 +0530

    setup a server

--- app.js
+++ app.js
@@ -4,9 +4,3 @@
 // consolconste.log(newItem)
 // console.log('hello people')
 
-const http = require('http')
-const server = http.createServer()
-server.on('request', (req, res)=>{
-  res.end('welcome')
-})
-server.listen(5000)
