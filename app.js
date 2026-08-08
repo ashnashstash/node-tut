22441bb2206d3be5dc681c9e1b16236e1362462d
Nashrah <nashrah0607@gmail.com>
2026-08-08 09:46:07 +0530

    async patterns

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const _ = require('lodash')
-const item = [1, [2, [3, [4, [5]]]]]
-const newItem = _.flattenDeep(item)
-console.log(newItem)
-console.log('hello people')
+// const _ = require('lodash')
+// const item = [1, [2, [3, [4, [5]]]]]
+// const newItem = _.flattenDeep(item)
+// consolconste.log(newItem)
+// console.log('hello people')
+
 
