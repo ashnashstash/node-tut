f2cf597f88983f3062034f3da092cc8cca8af8b0
Nashrah <nashrah0607@gmail.com>
2026-08-09 10:23:45 +0530

    http-html

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-const _ = require('lodash')
-const item = [1, [2, [3, [4, [5]]]]]
-const newItem = _.flattenDeep(item)
-console.log(newItem)
-console.log('hello people')
+// const _ = require('lodash')
+// const item = [1, [2, [3, [4, [5]]]]]
+// const newItem = _.flattenDeep(item)
+// console.log(newItem)
+// console.log('hello people')
+
+const http = require('http')
+const {readFileSync} = require('fs')
+
+const homePage = readFileSync('./index.html')
+
+const server = http.createServer((req, res)=>{
+  //console.log(req.method)
+  //console.log(req.url)
+  const url = req.url
+
+  if(url === '/'){
+    res.writeHead(200, {'content-type' : 'text/html'})
+    res.write(homePage)
+    res.end()
+  }
+  else if(url === '/about'){
+    res.writeHead(200, {'content-type' : 'text/html'})
+    res.write(`<h1>about page</h1>`)
+    res.end()
+  }
+  else{
+    res.writeHead(404, {'content-type' : 'text/html'})
+    res.write(`<h1>error 404</h1>`)
+    res.end()
+  }
+})
+
+server.listen(5000)
