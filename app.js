33f565b4756fd1fa3c967ed956ab68857a046387
Nashrah <nashrah0607@gmail.com>
2026-08-09 11:03:39 +0530

    html app

--- app.js
+++ app.js
@@ -4,31 +4,3 @@
 // console.log(newItem)
 // console.log('hello people')
 
-const http = require('http')
-const {readFileSync} = require('fs')
-
-const homePage = readFileSync('./navbar-app/index.html')
-
-const server = http.createServer((req, res)=>{
-  //console.log(req.method)
-  //console.log(req.url)
-  const url = req.url
-
-  if(url === '/'){
-    res.writeHead(200, {'content-type' : 'text/html'})
-    res.write(homePage)
-    res.end()
-  }
-  else if(url === '/about'){
-    res.writeHead(200, {'content-type' : 'text/html'})
-    res.write(`<h1>about page</h1>`)
-    res.end()
-  }
-  else{
-    res.writeHead(404, {'content-type' : 'text/html'})
-    res.write(`<h1>error 404</h1>`)
-    res.end()
-  }
-})
-
-server.listen(5000)
