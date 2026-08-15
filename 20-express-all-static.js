84326ea1a9f8c054e8736651b5247ae1b18b6e17
Nashrah <nashrah0607@gmail.com>
2026-08-15 21:14:49 +0530

    express all static

---
+++ 20-express-all-static.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('./public'))
+
+// app.get('/', (req, res)=>{
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
+// adding to static asserts
+// server side rendering(SSR)
+
+app.all('{*splat}', (req, res)=>{
+  res.status(404).send('Resource not found')
+})
+
+app.listen(5000, ()=>{  
+  console.log('Server is listening on port 5000...')
+})
